3175c199506dbe09dddd0a84aaadec90415fbc54
Dinesh <dinesh.kriishnan@gmail.com>
2017-12-17 12:35:21 +0530

    Added entities

---
+++ BusinessCardScanner.Cognitive/Entities/ContactCard.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace BusinessCardScanner.Cognitive.Entities
+{
+    public class ContactCard
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Position { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public string Website { get; set; }
+        public string Facebook { get; set; }
+        public string Twitter { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Name: " + Name);
+            sb.AppendLine("Company: " + Company);
+            sb.AppendLine("Position: " + Position);
+            sb.AppendLine("Phone: " + PhoneNo);
+            sb.AppendLine("Email: " + Email);
+            sb.AppendLine("Website: " + Website);
+            sb.AppendLine("Facebook: " + Facebook);
+            sb.AppendLine("Twitter: " + Twitter);
+
+            return sb.ToString();
+        }
+    }
+}

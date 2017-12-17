c9d5574a699f8c82f92bf12bd29101c489ac0a7e
Dinesh <dinesh.kriishnan@gmail.com>
2017-12-17 12:35:31 +0530

    Added constants

---
+++ BusinessCardScanner.Cognitive/Common/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace BusinessCardScanner.Cognitive.Common
+{
+    public class Constants
+    {
+        public const string CognitiveUrl = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0";
+        public const string CognitiveKeyHeader = "Ocp-Apim-Subscription-Key";
+        public const string CognitiveKey = "03103390f4644b52b25ba2cb88e4851f";
+        public const string OctetStreamHeader = "application/octet-stream";
+    }
+}

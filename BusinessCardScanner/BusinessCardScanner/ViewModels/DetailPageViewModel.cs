76d2d5b01ed44d7b7f93c8a6991bfa639af1a9c6
Dinesh <dinesh.kriishnan@gmail.com>
2017-12-17 17:49:36 +0530

    Added detail viewmodel and view

---
+++ BusinessCardScanner/BusinessCardScanner/ViewModels/DetailPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using BusinessCardScanner.Cognitive.Entities;
+using Prism.Navigation;
+
+namespace BusinessCardScanner.ViewModels
+{
+    public class DetailPageViewModel : ViewModelBase
+    {
+        private readonly INavigationService _navigationService;
+        public DetailPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public ContactCard ContactDetails { get; set; } = new ContactCard();
+
+        public override void OnNavigatedTo(NavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            ContactDetails = parameters["CardDetails"] as ContactCard;
+            RaisePropertyChanged(nameof(ContactDetails));
+        }
+    }
+}
